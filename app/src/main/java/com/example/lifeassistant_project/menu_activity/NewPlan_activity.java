145b6314eafe784f5bcc2c15b0ca9dc62e3a3e89
yezishuo <stevenyutsunei@gmai.com>
2020-06-11 15:47:37 +0800

    1547NEWPLAN

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/NewPlan_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/NewPlan_activity.java
@@ -2,16 +2,67 @@ package com.example.lifeassistant_project.menu_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.example.lifeassistant_project.R;
 
-public class NewPlan_activity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
+public class NewPlan_activity extends AppCompatActivity {
+    private static final String PATH = "/data/data/com.example.lifeassistant_project";
+    private static final String DBNAME = "myDB.db";
+    private static final String FILTER_TABLE = "filter";
+    private static final String BK_TABLE = "record";
+
+    private SQLiteDatabase myDB;
+    private Cursor cursor;
+    ArrayList<String> stuffList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_new_plan_activity);
+        File dbDir = new File(PATH, "databases");
+        dbDir.mkdir();
+        File FdbFile = new File(PATH+"/databases",DBNAME);
+        if(!FdbFile.exists() || !FdbFile.isFile())
+            copyAssets(PATH); //初始資料庫複製到路徑
+
+
+
+
     }
+    private void copyAssets(String path) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = getAssets().open(DBNAME);
+            out = new FileOutputStream(PATH + "/databases/" + DBNAME);
+            copyFile(in, out);
+            in.close();
+            out.flush();
+            out.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+     * 一既有的工具程式，可將來源 InputStream 物件所指向的資料串流
+     * 拷貝到OutputStream 物件所指向的資料串流去
+     */
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[in.available()];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
 }
