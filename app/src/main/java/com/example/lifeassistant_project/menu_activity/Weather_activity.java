0cd05fa4aa34fe53fe46a3d9ac41e7e33623ccf4
yezishuo <stevenyutsunei@gmail.com>
2020-07-15 16:55:33 +0800

    07/15:1655

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/Weather_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/Weather_activity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.lifeassistant_project.R;
+
+import java.util.*;
 
 public class Weather_activity extends AppCompatActivity {
 
@@ -13,7 +16,27 @@ public class Weather_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); //隱藏狀態列(綠色的那塊)
-        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);  //全螢幕
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);  //全螢幕
         setContentView(R.layout.activity_weather_activity);
+        ArrayList<String> weather_condition = new ArrayList();
+        ArrayList<Integer> weather_degree_higher = new ArrayList();
+        ArrayList<Integer> weather_degree_lower = new ArrayList();
+
+        ArrayList weather_thismorning = new ArrayList<String>();
+        weather_condition.add("晴時多雲");
+        weather_degree_higher.add(27);
+        weather_degree_lower.add(14);
+        System.out.println("Contents of al: " +weather_thismorning.get(1) );
+        TextView currentcondition = (TextView)findViewById(R.id.conditions);
+        currentcondition.setText(weather_condition.get(0));
+
+
+
+
+
+
     }
+
+
+
 }
