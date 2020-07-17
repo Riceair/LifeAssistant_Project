58ec6f72f1f9ce289f6208332a2894e09548ba01
yezishuo <stevenyutsunei@gmail.com>
2020-07-17 18:08:43 +0800

    1808

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/Login_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/Login_activity.java
@@ -3,6 +3,7 @@ package com.example.lifeassistant_project.menu_activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lifeassistant_project.R;
 
@@ -11,6 +12,8 @@ public class Login_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //隱藏狀態列(綠色的那塊)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);  //全螢幕
         setContentView(R.layout.activity_login_activity);
     }
 }
