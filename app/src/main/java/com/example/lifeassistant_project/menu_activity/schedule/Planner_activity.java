9ea1c09eae710f8fbce42132e60adede45869fd2
yezishuo <stevenyutsunei@gmail.com>
2020-07-24 18:31:17 +0800

    1831

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/Planner_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/Planner_activity.java
@@ -4,23 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lifeassistant_project.R;
 import com.example.lifeassistant_project.menu_activity.finance.Report_type_activity;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Planner_activity extends AppCompatActivity {
     private static final String TAG = "Planner_activity";
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    private SimpleDateFormat dateFormatForDisplaying = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", Locale.getDefault());
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+    private boolean shouldShow = false;
+    private CompactCalendarView compactCalendarView;
+    private static final String PATH = "/data/data/com.example.lifeassistant_project";
+    private static final String DBNAME = "myDB.db";
+    private static final String FILTER_TABLE = "filter";
+    private static final String BK_TABLE = "record";
+    private SQLiteDatabase myDB;
+    private Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +58,20 @@ public class Planner_activity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setIcon(R.drawable.newstand);
+        final TextView monthlayout = (TextView) findViewById(R.id.Month_layout);
         final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        //這是資料庫
+        File dbDir = new File(PATH, "databases");
+        dbDir.mkdir();
+        File FdbFile = new File(PATH+"/databases",DBNAME);
+        if(!FdbFile.exists() || !FdbFile.isFile())
+            copyAssets(PATH); //初始資料庫複製到路徑
+
+        ReadDBRecord();
+        ArrayList<String> stuffList = new ArrayList<>();
+        ///////////
+
+
         // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
         // Use constants provided by Java Calendar class
         compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
@@ -64,10 +103,61 @@ public class Planner_activity extends AppCompatActivity {
             @Override
             public void onMonthScroll(Date firstDayOfNewMonth) {
                 Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
+                monthlayout.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
             }
         });
+        monthlayout.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+    }
+    private void ReadDBRecord(){
+        myDB = openOrCreateDatabase(DBNAME, MODE_PRIVATE, null);
+        try {
+            cursor = myDB.rawQuery("select schedule_record.事情, schedule_record.年,schedule_record.月,schedule_record.日,schedule_record.開始時間,schedule_record.結束時間 from schedule_record",null);
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            if(cursor!=null) {
+                int iRow = cursor.getCount(); // 取得資料記錄的筆數
+                cursor.moveToFirst();
+                for (int i=0;i<iRow;i++){
+                    String stuffTime = cursor.getString(0)+" ("+cursor.getString(1)+"年"+cursor.getString(2)+"月"+cursor.getString(3)+"日) （時間：Starts at"+cursor.getString(4)+"點/ Ends at: "+cursor.getString(5)+"點)";
+                    cursor.moveToNext();
+                }
+
+                // 5. 關閉 DB
+                myDB.close();
+            }
+            else {
+                Toast.makeText(this,"Hint 1: 請將db準備好!",Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
     }
 
+    private void copyAssets(String path) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = getAssets().open(DBNAME);
+            out = new FileOutputStream(PATH + "/databases/" + DBNAME);
+            copyFile(in, out);
+            in.close();
+            out.flush();
+            out.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+     * 一既有的工具程式，可將來源 InputStream 物件所指向的資料串流
+     * 拷貝到OutputStream 物件所指向的資料串流去
+     */
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[in.available()];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
     public void clickNewPlan(View view){
         Intent intent = new Intent(view.getContext(),NewPlan_activity.class);
         view.getContext().startActivity(intent);
