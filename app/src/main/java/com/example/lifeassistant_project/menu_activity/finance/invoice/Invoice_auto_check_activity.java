9ebe9c3bf5bed215a301144a04445fdc83ae822a
Riceair <handy00095@gmail.com>
2020-09-01 17:35:36 +0800

    動畫bug修正 Invoice修改

---
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/finance/invoice/Invoice_auto_check_activity.java
@@ -0,0 +1,16 @@
+package com.example.lifeassistant_project.menu_activity.finance.invoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lifeassistant_project.R;
+
+public class Invoice_auto_check_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_auto_check_activity);
+    }
+}
