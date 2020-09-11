f10ee5c2acabd92cd3e0ff7cfa9966d26a88ef45
yezishuo <stevenyutsunei@gmail.com>
2020-09-11 17:26:02 +0800

    Planner Tablet 細節成就完美

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/NewPlan_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/NewPlan_activity.java
@@ -190,13 +190,13 @@ public class NewPlan_activity extends AppCompatActivity {
         return String.valueOf(year) + "-" + String.format("%02d", monthOfYear + 1) + "-" + String.format("%02d", dayOfMonth);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            NewPlan_activity.this.finish();
-        }
-        return true;
-    }
+//q
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == android.R.id.home) {
+//            NewPlan_activity.this.finish();
+//        }
+//        return true;
+//    }
 
     public void setStartTime(View v) {
         timePickerDialog.show();
