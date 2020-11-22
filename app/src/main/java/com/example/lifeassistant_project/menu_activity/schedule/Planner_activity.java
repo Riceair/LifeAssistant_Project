2cd0d45db57722f402214e6f4eb03f80e4c5677f
yezishuo <stevenyutsunei@gmail.com>
2020-11-22 13:54:08 +0800

    排程頁面顯示導覽條 始動！

--- app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/Planner_activity.java
+++ app/src/main/java/com/example/lifeassistant_project/menu_activity/schedule/Planner_activity.java
@@ -91,7 +91,7 @@ public class Planner_activity extends AppCompatActivity {
 //        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
 //            @Override
 //            public void onSystemUiVisibilityChange(int visibility) {
-//                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+//               if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
 //                    decorView.setSystemUiVisibility(
 //                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 //                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -570,13 +570,13 @@ public class Planner_activity extends AppCompatActivity {
         initial(null);
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        final int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        final View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(uiOptions);
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        final int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        final View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(uiOptions);
+//    }
     public void clickViewPlan(View view){
         selectstatus=0;
         Intent intent = new Intent(view.getContext(),ViewPlan_activity.class);
